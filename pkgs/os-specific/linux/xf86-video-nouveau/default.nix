e5c8757bc983f8d6e80f5314d4f76bc7e657fd8a
aszlig <aszlig@redmoonstudios.org>
2012-10-21 11:42:20 +0200

    xf86-video-nouveau: Update to version 1.0.2.
    This is not directly related to the Mesa upgrade, but to the libdrm upgrade.
    In addition, this needs to have pixman include paths and buildInputs.
    
    And we no longer need to fetchgit here, as the project now has normal releases.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/os-specific/linux/xf86-video-nouveau/default.nix
+++ pkgs/os-specific/linux/xf86-video-nouveau/default.nix
@@ -1,31 +1,33 @@
 { stdenv
-, fetchgit
+, fetchurl
 , autoconf
 , automake
 , libtool
-, xorgserver, xproto, fontsproto, xf86driproto, renderproto, videoproto
+, xorgserver, xproto, fontsproto, xf86driproto, renderproto, videoproto, pixman
 , utilmacros
 , libdrm
 , pkgconfig }:
 
-stdenv.mkDerivation {
-  name = "xf86-video-nouveau-2012-03-05";
+stdenv.mkDerivation rec {
+  name = "xf86-video-nouveau-${version}";
+  version = "1.0.2";
 
-  src = fetchgit {
-    url = git://anongit.freedesktop.org/nouveau/xf86-video-nouveau;
-    rev = "f5d1cd2cb6808838ae1a188cef888eaa9582c76d";
-    sha256 = "8c20e9ce7897fbd4c5097e4738e80ecca30e6326b758a13fc97f96ccc12fd7d9"; 
+  src = fetchurl {
+    url = "http://nouveau.freedesktop.org/release/${name}.tar.bz2";
+    sha256 = "0pf4rg62cbm4y49rir17yxcb2w5g326i4yjic11lshqxm7132mvn";
   };
 
   buildInputs = [
     autoconf
     automake
     libtool
-    xorgserver xproto fontsproto xf86driproto renderproto videoproto
+    xorgserver xproto fontsproto xf86driproto renderproto videoproto pixman
     utilmacros
     libdrm
     pkgconfig
   ];
+
+  NIX_CFLAGS_COMPILE = "-I${pixman}/include/pixman-1";
 
   preConfigure = "autoreconf -vfi";
 
