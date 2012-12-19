bbb6d423217f26f0f993eff5b084608aed6c63f6
aszlig <aszlig@redmoonstudios.org>
2012-12-19 17:54:11 +0100

    steam: Base package on Steam internal packages.
    So far we used the steam binary from the Debian package. This isn't really
    needed as the binary as well as all those scripts are mostly used for
    bootstrapping only.
    
    The new approach is fetching _all_ required files directly from
    http://media.steampowered.com/ and create a fake package and .installed file.
    
    I'm not yet 100% clear what the .installed file _should_ contain, but after some
    debugging I found out that it mostly contains paths and the corresponding file
    sizes. The format in detail is like this:
    
    <path>,<file_size>\n
    
    If the path is a directory, file size is -1.
    
    But it doesn't seem to be the whole truth as there are library calls to strchr()
    even after the file size. In addition there are even strchr() calls operating on
    ':' so it might be useful if we would have an actual example file.
    
    In addition, the new appreach now fetches all resource files needed for Steam,
    so we could possibly avoid updating in the first place and already have a
    readily bootstrapped Steam in the derivation output.
    
    So, while working on packaging Steam it came to my attention (a bit too late if
    you ask me), that @cpages is also working on packaging Steam. He began way
    earlier (11 days ago, so about week before I started off) and might have better
    chances of success because he actually has closed beta access.
    
    Well, but the more experts we have on a particular package the better. It's just
    a shame of the duplicate work, so sorry @cpages for not noticing your branch.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/games/steam/default.nix
+++ pkgs/games/steam/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchurl, unzip, makeWrapper, libX11
+{ stdenv, fetchurl, unzip, makeWrapper, libX11, writeText
 , zlib, pango, glib, mesa
-, libXrandr, libXext, libXfixes, libXi, libXrender
+, libXrandr, libXext, libXfixes, libXi, libXrender, libXinerama
 , cups, cairo, freetype, fontconfig, openal, nss, gtk2, gdk_pixbuf
 , alsaLib, pulseaudio, nspr, dbus, libpng12, libgcrypt
 }:
@@ -8,66 +8,118 @@
 with stdenv.lib;
 
 let
-  fetchSteam = pkg: sha1: fetchurl {
+  fetchSteam = pkg: sha1: (fetchurl {
     inherit sha1;
+    name = "${pkg}.zip";
     url = "http://media.steampowered.com/client/${pkg}.zip.${sha1}";
+  }) // {
+    inherit pkg sha1;
   };
 
-  steamPackages = [
-    (fetchSteam "bins_ubuntu12"   "c5b2136df4191824b6a4239193aaa49f75294d68")
-    (fetchSteam "webkit_ubuntu12" "2afa14b88266fbb01846ce0eeded8a0c67767b37")
-    (fetchSteam "sdl2_ubuntu12"   "ef2498ac145cf54eede07acdb74473afd39f170f")
+  version = "1355250651";
+
+  srcs = [
+    (fetchSteam "strings_all"
+                "9de2aafbac6a2b87810a2ba34d2535e39815131c")
+    (fetchSteam "resources_all"
+                "363599fd9b416988a0e6e7dfb466fdb5111ab7c3")
+    (fetchSteam "tenfoot_all"
+                "decc1a5ea469abd3af5d2bbd6d63e22065e303a1")
+    (fetchSteam "tenfoot_fonts_all"
+                "a155ceac3231ac6e40345fd45c6c7c8213920018")
+    (fetchSteam "tenfoot_ambientsounds_all"
+                "ac31503c42f094976f64844d8ff54705cadc7dd3")
+    (fetchSteam "tenfoot_sounds_all"
+                "e8cce9ed600fc09e4a3f510fc3595d515bba6cd6")
+    (fetchSteam "tenfoot_images_all"
+                "77918e672284f4e512d805fda98452b2cdda33f1")
+    (fetchSteam "public_all"
+                "59f6218bb080590b710237c10c96f2fe8d431f41")
+    (fetchSteam "bins_ubuntu12"
+                "9a078431a78d549867dbe4574207f908042ad9d5")
+    (fetchSteam "webkit_ubuntu12"
+                "2afa14b88266fbb01846ce0eeded8a0c67767b37")
+    (fetchSteam "miles_ubuntu12"
+                "9c547c676be26625a4cd64c2595821d10b8fc9ff")
+    (fetchSteam "sdl2_ubuntu12"
+                "ef2498ac145cf54eede07acdb74473afd39f170f")
+    (fetchSteam "steam_ubuntu12"
+                "c84c824580aa0912ea56805ea5af97db2a21239a")
   ];
 
-in stdenv.mkDerivation rec {
-  name = "steam-${version}";
-  version = "1.0.0.17beta";
+  mkFakePackageItem = src: ''
+    "${src.pkg}"
+    {
+      "file" "${src}"
+      "checksum" "${src.sha1}"
+      "size" "$(stat -c '%s' "${src}")"
+    }
+  '';
 
-  src = fetchurl {
-    url = "http://repo.steampowered.com/steam/pool/steam/s/steam/"
-        + "steam_1.0.0.17_i386.deb";
-    sha256 = "1mknhr9xgaqfqp7p449w974ias8g69yzgjh3z4zyzci6ccfb8dp1";
-  };
+  fakePackageBuilder = writeText "steampkg.sh" ''
+    cat <<STEAMPKG
+    "ubuntu12"
+    {
+      "version" "${version}"
+      ${concatMapStrings mkFakePackageItem srcs}
+    }
+    STEAMPKG
+  '';
+
+in stdenv.mkDerivation {
+  name = "steam-${version}";
+  inherit version srcs;
 
   buildInputs = [ unzip makeWrapper ];
 
-  unpackCmd = ''
-    ar p "$curSrc" data.tar.gz |
-      tar xz ./usr/lib/steam/bootstraplinux_ubuntu12_32.tar.xz -O |
-      tar xJ ubuntu12_32/steam
+  unpackPhase = ''
+    sourceRoot="steam-${version}"
+    for archive in $srcs; do
+      unzip -qqu "$archive" -d "$sourceRoot"
+    done
+    chmod -R u+w "$sourceRoot"
   '';
-
-  postUnpack = flip map steamPackages (pkg: ''
-    unzip "${pkg}" 'ubuntu12_32/*'
-  '');
 
   dontStrip = true;
 
   buildPhase = let
     rpath = makeLibraryPath [
       stdenv.gcc.gcc libX11 zlib pango glib mesa
-      libXrandr libXext libXfixes libXi libXrender
+      libXrandr libXext libXfixes libXi libXrender libXinerama
       cups cairo freetype fontconfig openal nss gtk2 gdk_pixbuf
       alsaLib pulseaudio nspr dbus libpng12 libgcrypt
     ];
   in ''
-    patchelf \
-      --set-interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
-      --set-rpath "${rpath}" \
-      steam
+    libexec="$out/libexec/steam"
 
-    for libfile in *.so*; do
-      patchelf --set-rpath "${rpath}" "$libfile"
-      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:." ldd "$libfile"
+    find ubuntu12_32 -type f -exec chmod +x '{}' +
+
+    for binfile in steam gameoverlayui; do
+      patchelf \
+        --set-interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
+        --set-rpath "$libexec/ubuntu12_32:${rpath}" \
+        "ubuntu12_32/$binfile"
+    done
+
+    for libfile in ubuntu12_32/*.so*; do
+      patchelf --set-rpath "$libexec/ubuntu12_32:${rpath}" "$libfile"
     done
   '';
 
   installPhase = ''
-    libexec="$out/libexec/steam"
-    install -vt "$libexec/bin" steam
-    cp -vr -t "$libexec" *.so*
+    mkdir -vp "$libexec"
+    cp -avt "$libexec" *
 
-    makeWrapper "$libexec/bin/steam" "$out/bin/steam" \
-      --set LD_LIBRARY_PATH "$libexec"
+    installed_data="$(find "$libexec"    \
+         \( -type f -printf '%p,%s\n' \) \
+      -o \( -type d -printf '%p,-1\n' \)
+    )"
+
+    mkdir -vp "$libexec/package"
+    echo "$installed_data" > "$libexec/package/steam_client_ubuntu12.installed"
+    sh "${fakePackageBuilder}" > "$libexec/package/steam_client_ubuntu12"
+
+    makeWrapper "$libexec/ubuntu12_32/steam" "$out/bin/steam" \
+      --set LD_LIBRARY_PATH "$libexec/ubuntu12_32"
   '';
 }
