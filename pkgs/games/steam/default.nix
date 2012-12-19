a0f49c8563482fdadef16f6053ac0bd4f1deae3a
aszlig <aszlig@redmoonstudios.org>
2012-12-19 12:43:56 +0100

    steam: Add libraries and all dependencies.
    We're going to fetch this directly from the media server which is used for
    updating Steam packages, obviously. Patch in RPATHs for every single shared
    object. This isn't really as selective as we really like to, but at least should
    get us started.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/games/steam/default.nix
+++ pkgs/games/steam/default.nix
@@ -1,6 +1,25 @@
-{ stdenv, fetchurl, makeWrapper, libX11 }:
+{ stdenv, fetchurl, unzip, makeWrapper, libX11
+, zlib, pango, glib, mesa
+, libXrandr, libXext, libXfixes, libXi, libXrender
+, cups, cairo, freetype, fontconfig, openal, nss, gtk2, gdk_pixbuf
+, alsaLib, pulseaudio, nspr, dbus, libpng12, libgcrypt
+}:
 
-stdenv.mkDerivation rec {
+with stdenv.lib;
+
+let
+  fetchSteam = pkg: sha1: fetchurl {
+    inherit sha1;
+    url = "http://media.steampowered.com/client/${pkg}.zip.${sha1}";
+  };
+
+  steamPackages = [
+    (fetchSteam "bins_ubuntu12"   "c5b2136df4191824b6a4239193aaa49f75294d68")
+    (fetchSteam "webkit_ubuntu12" "2afa14b88266fbb01846ce0eeded8a0c67767b37")
+    (fetchSteam "sdl2_ubuntu12"   "ef2498ac145cf54eede07acdb74473afd39f170f")
+  ];
+
+in stdenv.mkDerivation rec {
   name = "steam-${version}";
   version = "1.0.0.17beta";
 
@@ -18,20 +37,35 @@ stdenv.mkDerivation rec {
       tar xJ ubuntu12_32/steam
   '';
 
+  postUnpack = flip map steamPackages (pkg: ''
+    unzip "${pkg}" 'ubuntu12_32/*'
+  '');
+
   dontStrip = true;
 
   buildPhase = let
-    rpath = stdenv.lib.makeLibraryPath [ stdenv.gcc.gcc libX11 ];
+    rpath = makeLibraryPath [
+      stdenv.gcc.gcc libX11 zlib pango glib mesa
+      libXrandr libXext libXfixes libXi libXrender
+      cups cairo freetype fontconfig openal nss gtk2 gdk_pixbuf
+      alsaLib pulseaudio nspr dbus libpng12 libgcrypt
+    ];
   in ''
     patchelf \
       --set-interpreter "$(cat $NIX_GCC/nix-support/dynamic-linker)" \
       --set-rpath "${rpath}" \
       steam
+
+    for libfile in *.so*; do
+      patchelf --set-rpath "${rpath}" "$libfile"
+      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:." ldd "$libfile"
+    done
   '';
 
   installPhase = ''
     libexec="$out/libexec/steam"
     install -vt "$libexec/bin" steam
+    cp -vr -t "$libexec" *.so*
 
     makeWrapper "$libexec/bin/steam" "$out/bin/steam" \
       --set LD_LIBRARY_PATH "$libexec"
