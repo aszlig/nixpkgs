3cbc09178a9ed4a5182018a21af5d5d931f5ee89
aszlig <aszlig@redmoonstudios.org>
2016-10-12 23:18:50 +0200

    fixup! chromium: Add preliminary support for GN

--- pkgs/applications/networking/browsers/chromium/common-gn.nix
+++ pkgs/applications/networking/browsers/chromium/common-gn.nix
@@ -133,6 +133,7 @@ let
       linux_use_bundled_binutils = false;
       linux_use_bundled_gold = false;
       linux_use_gold_flags = true;
+      is_debug = false;
 
       proprietary_codecs = false;
       use_sysroot = false;
